e70307bd04465b5e6c5d7fa0aea1e17f4b4a316f
maverickdeepak <imdeepakchauhan12@gmail.com>
2025-10-11 22:18:35 +0530

    integrate husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,49 @@
 
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
+export default tseslint.config(
     eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
     {
-        ignores: ['.eslintrc', 'dist', 'node_modules', 'eslint.config.mjs'],
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-            },
-        },
         rules: {
-            'no-console': 'off',
-            'dot-notation': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_' },
+            ],
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-var-requires': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+            '@typescript-eslint/no-namespace': 'off',
+            '@typescript-eslint/no-use-before-define': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-empty-interface': 'off',
+            '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
         },
     },
-])
+    {
+        ignores: ['**/dist/**/*'],
+    },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parserOptions: {
+                project: ['tsconfig.json'],
+            },
+        },
+    },
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            globals: {
+                React: 'readonly',
+            },
+        },
+    },
+)
