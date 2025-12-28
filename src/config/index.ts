20b348886a58681ce4c84b6f006f7d4fe1f4b61b
maverickdeepak <imdeepakchauhan12@gmail.com>
2025-12-28 13:04:03 +0530

    add TypeORM setup with sample user entity, database config, and initialization logic

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,18 @@
 import { config } from 'dotenv'
+import path from 'node:path'
 
-config()
+config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+})
 
-const { PORT, NODE_ENV } = process.env
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } =
+    process.env
 export const Config = {
     PORT: PORT || '5501',
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_USERNAME,
+    DB_PASSWORD,
 }
