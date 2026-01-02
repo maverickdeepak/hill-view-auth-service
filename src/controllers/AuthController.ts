ddb480ce5f793318cb557faa41ba48fb0f748756
maverickdeepak <imdeepakchauhan12@gmail.com>
2026-01-02 23:52:09 +0530

    enhance `/auth/register`: update `express-validator` to schema-based validation, improve error handling in `AuthController`, and expand tests

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,7 +2,6 @@ import type { NextFunction, Response } from 'express'
 import { RegisterUserRequestBody } from '../types'
 import { UserService } from '../service/UserService'
 import Logger from '../config/logger'
-import createHttpError from 'http-errors'
 import { validationResult } from 'express-validator'
 
 export class AuthController {
@@ -19,9 +18,7 @@ export class AuthController {
         const errors = validationResult(req)
 
         if (!errors.isEmpty()) {
-            const err = createHttpError(400, 'Email is required')
-            next(err)
-            return
+            return res.status(400).json({ errors: errors.array() })
         }
 
         const { firstName, lastName, email, password } = req.body
--- src/validators/register-validators.ts
+++ src/validators/register-validators.ts
@@ -1,7 +1,21 @@
-import { body } from 'express-validator'
+import { checkSchema } from 'express-validator'
 
-export default [
-    body('email').notEmpty(),
-    body('password').notEmpty(),
-    body('firstName').notEmpty(),
-]
+export default checkSchema({
+    firstName: {
+        isLength: {
+            options: { min: 3 },
+            errorMessage: 'First name should be at least 3 chars',
+        },
+    },
+    email: {
+        errorMessage: 'Email is required',
+        isEmail: true,
+        notEmpty: true,
+    },
+    password: {
+        isLength: {
+            options: { min: 8 },
+            errorMessage: 'Password should be at least 8 chars',
+        },
+    },
+})
